03cb90414cf66cd70a9c357552e5c4cf290337b1
Jeremy Lua <jeremy.lua@imc.com>
2020-12-02 09:05:36 -0600

    Extracted out problem 1 into new folder

---
+++ 2020/1/Main1.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main1
+{
+    public static void main(String[] anArguments)
+    {
+        List<Integer> aList = parseInput("/Users/jlua/Documents/ForFun/2020/1/input.txt");
+        print(get2020(aList));
+    }
+
+    public static int get2020(List<Integer> aList)
+    {
+        for (int i = 0; i < aList.size(); ++i)
+        {
+            int myVal = aList.get(i);
+            for (int j = i + 1; j < aList.size(); ++j)
+            {
+                int mySecondVal = aList.get(j);
+                for (int k = j + 1; k < aList.size(); ++k)
+                {
+                    int myThirdVal = aList.get(k);
+                    if (myVal + mySecondVal + myThirdVal == 2020)
+                    {
+                        return myVal * mySecondVal * myThirdVal;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static List<Integer> parseInput(String aFilename)
+    {
+        List<Integer> myResult = new ArrayList<>();
+        try (Scanner myScanner = new Scanner(new File(aFilename)))
+        {
+            myScanner.useDelimiter("");
+            while (myScanner.hasNext())
+            {
+                myResult.add(Integer.parseInt(myScanner.nextLine()));
+            }
+        }
+        catch (Exception ignored)
+        {
+            print("File could not be opened");
+        }
+        return myResult;
+    }
+
+    public static void print(int aNum)
+    {
+        print(String.valueOf(aNum));
+    }
+
+    public static void print(String aMessage)
+    {
+        System.out.println(aMessage);
+    }
+}
--- 2020/2/Main2.java
+++ 2020/2/Main2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main
+public class Main2
 {
     public static void main(String[] anArguments)
     {
